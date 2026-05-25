92f1e4fa66e5e3e0103ec8ace6f17ff9918d863d
Taras <taraszabudsky@gmail.com>
2026-05-25 16:37:19 +0300

    1637

--- js/task-5.js
+++ js/task-5.js
@@ -1,9 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
 const button = document.querySelector(".change-color");
 const colorSpan = document.querySelector(".color");
 
